04c1d4af56483ad57f14bf1ac145ccc9b69b8493
furimu <63914180+furimu@users.noreply.github.com>
2020-04-25 20:06:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from discord.ext import commands
+from datetime import datetime
 
 bot = commands.Bot(command_prefix="p:")
 
+DEFAULT_MESSAGE = {}
 
+def save(value, module):
+    with open(f'{module}.json', 'w', encoding= 'utf-8') as f:
+        return json.dump(value, f, ensure_ascii=False, indent=4)
+
+
+def load(module):
+    try:
+        with open(f'{module}.json', encoding='utf-8') as f:
+            return json.load(f)
+
+    except OSError:
+        save(DEFAULT_MESSAGE, module)
+
+uc= load("user_count")
 
 @bot.event
 async def on_ready():
@@ -10,5 +26,20 @@ async def on_ready():
 
 @bot.event
 async on_voice_state_update(member, before, after):
+    dt_now = datetime.datetime.now()
+    if str(member.id) not in uc.keys():
+            uc[str(member.id)]= {}
+
+    if before.channel is None:
+        uc[str(member.id)]["start"] = str(dt_now.second)
+        save(uc, user_count)
+
     if after.channel is None:
-        
+       uc[str(member.id)]["end"] = str(dt_now.second)   
+       all = int(uc[str(member.id)]["end"]) - int(uc[str(member.id)]["start"])
+       uc[str(member.id)]["all"] = str(all)
+       save(uc, user_count)
+
+
+       
+       
