d15eee4ad4113d1a06d6fc4d5898240e74473f23
furimu <63914180+furimu@users.noreply.github.com>
2020-04-28 00:21:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ cogs = [
     'usercount'
 ]
 
+send_error_channel = bot.get_channel(663772130978168842)
+        await send_error_channel.send('新規エラー')
+        for cog in cogs:
+            try:
+                bot.load_extension(cog)
+            except commands.ExtensionAlreadyLoaded:
+                pass
+ 
+            except Exception:
+                await send_error_channel.send(f'```py\n{traceback.format_exc()}\n```')
+
+
 @bot.event
 async def on_ready():
     print("yes")
