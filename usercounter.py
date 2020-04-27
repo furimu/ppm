6f5b50d47022da2855bc9f76a28847df2312b13d
furimu <63914180+furimu@users.noreply.github.com>
2020-04-28 00:10:08 +0900

    Update usercounter.py

--- usercounter.py
+++ usercounter.py
@@ -4,41 +4,41 @@ class User_Count(commands.Cog):
     def __init(self, bot):
         self.bot = bot
 
-    @commands.Cog.lstener()
-async def on_voice_state_update(member, before, after):
-    dt_now = datetime.now()
-    if str(member.id) not in uc.keys():
+    @commands.Cog.listener()
+    async def on_voice_state_update(member, before, after):
+        dt_now = datetime.now()
+        if str(member.id) not in uc.keys():
             uc[str(member.id)]= {}
  
-    if before.channel is None:
-        uc[str(member.id)]["start"] = str(dt_now.second)
-        save(uc, "user_count")
+        if before.channel is None:
+            uc[str(member.id)]["start"] = str(dt_now.second)
+            save(uc, "user_count")
  
-    if after.channel is None:
-       uc[str(member.id)]["end"] = str(dt_now.second)   
-       if 'all' not in uc[str(member.id)].keys():
-           all = int(uc[str(member.id)]["end"]) - int(uc[str(member.id)]["start"])
+        if after.channel is None:
+           uc[str(member.id)]["end"] = str(dt_now.second)   
+           if 'all' not in uc[str(member.id)].keys():
+               all = int(uc[str(member.id)]["end"]) - int(uc[str(member.id)]["start"])
    
-       else:
-           all = int(uc[str(member.id)]["all"]) + (int(uc[str(member.id)]["end"]) - int(uc[str(member.id)]["start"]))
-       print(all)
-       uc[str(member.id)]["all"] = str(all)
-       save(uc, "user_count")
+           else:
+               all = int(uc[str(member.id)]["all"]) + (int(uc[str(member.id)]["end"]) - int(uc[str(member.id)]["start"]))
+
+           uc[str(member.id)]["all"] = str(all)
+           save(uc, "user_count")
  
  
-@bot.command()
-async def show(ctx):
-    member = ctx.author
-    if str(member.id) not in uc.keys():
-        return await ctx.send('貴方がvcに入ったデータはありません')
+    @commands.command()
+    async def show(ctx):
+        member = ctx.author
+        if str(member.id) not in uc.keys():
+            return await ctx.send('貴方がvcに入ったデータはありません')
  
-    elif 'all' not in uc[str(member.id)].keys():
-        return await ctx.send('貴方がvcに入った合計時間のデータはありません')
-    await ctx.send(f'現在までに貴方がvcに入った時間は`{uc[str(member.id)]["all"]}秒`です')
+        elif 'all' not in uc[str(member.id)].keys():
+            return await ctx.send('貴方がvcに入った合計時間のデータはありません')
+        await ctx.send(f'現在までに貴方がvcに入った時間は`{uc[str(member.id)]["all"]}秒`です')
  
  
-@bot.command()
-@commands.has_permissions(administrator = True)
+    @commands.command()
+commands.has_permissions(administrator = True)
 async def adshow(ctx, member: discord.Member):
     if str(member.id) not in uc.keys():
         return await ctx.send(f'{member.name}がvcに入ったデータはありません')
