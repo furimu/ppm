90999345458332d419cfd926b46dac5b86da19ad
furimu <63914180+furimu@users.noreply.github.com>
2020-04-28 00:22:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ cogs = [
 ]
 
 send_error_channel = bot.get_channel(663772130978168842)
-        await send_error_channel.send('新規エラー')
-        for cog in cogs:
-            try:
-                bot.load_extension(cog)
-            except commands.ExtensionAlreadyLoaded:
-                pass
+await send_error_channel.send('新規エラー')
+for cog in cogs:
+    try:
+        bot.load_extension(cog)
+    except commands.ExtensionAlreadyLoaded:
+        pass
  
-            except Exception:
-                await send_error_channel.send(f'```py\n{traceback.format_exc()}\n```')
+    except Exception:
+    await send_error_channel.send(f'```py\n{traceback.format_exc()}\n```')
 
 
 @bot.event
