f1751cb629073496a5c9f7fe62b22fd19d109cca
furimu <63914180+furimu@users.noreply.github.com>
2020-04-28 00:52:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ from datetime import datetime
 import discord
 import botinfo
 import json
+import traceback
 bot = commands.Bot(command_prefix="p:")
 
 cogs = [
     'usercounter'
 ]
 
-send_error_channel = bot.get_channel(663772130978168842)
-await send_error_channel.send('新規エラー')
 for cog in cogs:
     try:
         bot.load_extension(cog)
     except commands.ExtensionAlreadyLoaded:
         pass
  
-    except Exception:
-        await send_error_channel.send(f'```py\n{traceback.format_exc()}\n```')
+    except Exception:    print(traceback.format_exc())
 
 
 @bot.event
