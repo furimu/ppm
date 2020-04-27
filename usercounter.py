f06d061206f6c2cb89b97b42c86a43035669101f
furimu <63914180+furimu@users.noreply.github.com>
2020-04-28 01:12:20 +0900

    Update usercounter.py

--- usercounter.py
+++ usercounter.py
@@ -91,7 +91,7 @@ class User_Counter(commands.Cog):
                 m = ctx.guild.get_member(int(r))
                 count = count +1
                 e.add_field( 
-                    name= f"{count}: {m.mention}",
+                    name= f"{count}: {str(m)}",
                     value = str(uc[r]["all"]),
                     inline=True)
       
