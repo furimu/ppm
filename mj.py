9e72822dcb23aa10e37bded1f32a6460844474a8
furimu <63914180+furimu@users.noreply.github.com>
2020-05-08 17:16:28 +0900

    Create mj.py

---
+++ mj.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import discord
+
+class MJ(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="チーム分け")
+    async def team_sprit(self, ctx, role:discord.Role, limit:int)
+        """役職でチームをわけます""
+        d={}
+        c=1
+        
+        for member in role.members:
+        if c not in d.keys():
+            d[str(c)] = []
+
+        if len(d[str(c)] == limit:
+            c += 1
+            continue
+
+        
